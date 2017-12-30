5c09021420f619134f4e18f43f70c9044ce0887d
recepinanc <recepinancc@gmail.com>
2017-12-30 16:49:51 +0300

    Remove punctuation and digits

--- lib/cloud_text.rb
+++ lib/cloud_text.rb
@@ -1,5 +1,32 @@
 require "cloud_text/version"
 
 module CloudText
-  # Your code goes here...
+
+  #LANGUAGE = [:en, :tr, :fr]
+
+  def initialize(input, options)
+    @input = input
+    @options = options
+  end
+
+  def clean_text
+    #language = @options.fetch(:language, "en")
+    exclude_digits = @options.fetch(:exclude_digits, false)
+    if exclude_digits
+      remove_digits
+    else
+      remove_punctuation
+    end
+    puts @input
+  end
+
+  private
+
+  def remove_punctuation
+    @input = input.gsub(/\W|_/, ‘’)
+  end
+
+  def remove_digits
+    @input = input.gsub(/\W|_|\d/, ‘’)
+  end
 end
