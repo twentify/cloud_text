6f6a2523413d8713b465f2b30fe7136cc959bd18
Oguzhan Ozdemir <aoozdemir@live.com>
2018-01-02 21:53:18 +0300

    better version control

--- lib/cloud_text/version.rb
+++ lib/cloud_text/version.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 module CloudText
-  VERSION = "0.1.0"
+  MAJOR = 0
+  MINOR = 1
+  TINY  = 0
+  # PRE   = nil
+  
+  VERSION = [MAJOR, MINOR, TINY].compact.join('.')
 end
